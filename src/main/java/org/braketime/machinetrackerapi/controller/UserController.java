c1b62b277a874d3cdb3ad6faf799913171f259b5
Sumit <sumitsapkota0@gmail.com>
2026-01-22 17:31:20 -0600

    feat: get user by id

--- src/main/java/org/braketime/machinetrackerapi/controller/UserController.java
+++ src/main/java/org/braketime/machinetrackerapi/controller/UserController.java
@@ -4,15 +4,18 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.braketime.machinetrackerapi.Dtos.RegisterUserRequest;
+import org.braketime.machinetrackerapi.Dtos.UserCreateResponse;
 import org.braketime.machinetrackerapi.domain.User;
+import org.braketime.machinetrackerapi.exception.NotFoundException;
 import org.braketime.machinetrackerapi.mapper.UserResponseMapper;
+import org.braketime.machinetrackerapi.repository.UserRepository;
 import org.braketime.machinetrackerapi.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     private final UserResponseMapper userResponseMapper;
+    private final UserRepository userRepository;
 
     @PostMapping
     public ResponseEntity<?> createUser(@Valid @RequestBody RegisterUserRequest request) {
@@ -29,6 +33,25 @@ public class UserController {
         return ResponseEntity.ok(
                 userResponseMapper.toResponse(user)
         );
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getUsers(){
+        log.info("Get users api hit");
+        List<User> users = userRepository.getAllByIsActive(true);
+        List<UserCreateResponse> response = users.stream()
+                .map(userResponseMapper::toResponse).toList();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(
+            @PathVariable String id
+    ){
+        return userRepository.findById(id)
+                .map(userResponseMapper::toResponse)
+                .map(ResponseEntity::ok)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
     }
 
 }
--- src/main/java/org/braketime/machinetrackerapi/repository/UserRepository.java
+++ src/main/java/org/braketime/machinetrackerapi/repository/UserRepository.java
@@ -4,9 +4,11 @@ import org.braketime.machinetrackerapi.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User,String> {
     Optional<User> findByEmail(String email);
+    List<User> getAllByIsActive(boolean active);
 }
